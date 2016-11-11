130695b84416d1ad9df7f46127134950eb4ca73f
adhvalle <adhvalle@gmail.com>
2016-11-11 19:28:42 +0100

    firebase set up

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,20 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider) {
+	var config = {
+		apiKey: "AIzaSyBoq7j2QhB7-YnCnYuMTcBWcbUJZRwloPg",
+		authDomain: "contacts-manager-a5c89.firebaseapp.com",
+		databaseURL: "https://contacts-manager-a5c89.firebaseio.com",
+		storageBucket: "contacts-manager-a5c89.appspot.com",
+		messagingSenderId: "443209305949"
+	};
+	$firebaseRefProvider
+		.registerUrl({
+			default: config.databaseURL,
+			contacts: config.databaseURL + '/contacts'
+		});
+
+	firebase.initializeApp(config);
+  })
